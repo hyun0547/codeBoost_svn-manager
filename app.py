8cd38854031f5d4e6914d0b1a6d033690546183b
hyun0547 <hyun0547@gmail.com>
2025-03-31 16:37:44 +0900

    create 수정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import os
 import subprocess
 
@@ -11,10 +11,10 @@ def create_svn_repository(repo_name, repo_base_path, config_path):
 
     # 리포지토리 이름과 경로가 유효한지 확인
     if not repo_name:
-        return {"error": "리포지토리 이름이 필요합니다."}, 400
+        return make_response(jsonify({"error": "리포지토리 이름이 필요합니다."}), 400)
 
     if os.path.isdir(repo_path):
-        return {"error": f"이미 존재하는 리포지토리: {repo_path}"}, 400
+        return make_response(jsonify({"error": f"이미 존재하는 리포지토리: {repo_path}"}), 400)
 
     try:
         # SVN 리포지토리 생성
@@ -25,11 +25,10 @@ def create_svn_repository(repo_name, repo_base_path, config_path):
             os.remove(conf_file)
 
         os.symlink(config_path, conf_file)
-        return {"message": f"SVN 리포지토리 생성 완료: {repo_path}"}, 200
+        return make_response(jsonify({"message": f"SVN 리포지토리 생성 완료: {repo_path}"}), 200)
 
     except subprocess.CalledProcessError as e:
-        return {"error": f"오류 발생: {e}"}, 500
-
+        return make_response(jsonify({"error": f"오류 발생: {e}"}), 500)
 
 def list_svn_repositories(repo_base_path="/srv/svn/repository", page_num=1, page_size=10):
     try:
@@ -51,13 +50,13 @@ def create_repo():
     data = request.get_json()
 
     if 'name' not in data:
-        return jsonify({"error": "리포지토리 이름이 필요합니다."}), 400
+        return make_response(jsonify({"error": "리포지토리 이름이 필요합니다."}), 400)
 
     repo_name = data['name']
     repo_base_path = "/srv/svn/repository"
     config_path = "/srv/svn/common/config/svnserve.conf"
 
-    return jsonify(create_svn_repository(repo_name, repo_base_path, config_path))
+    return create_svn_repository(repo_name, repo_base_path, config_path)
 
 
 @app.route('/repo_list', methods=['POST'])
